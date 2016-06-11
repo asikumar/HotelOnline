a28cd8a643c024750fdbcbc2d8e38d011cc27b2d
asikumar <asit279@gmail.com>
2016-06-11 13:28:21 +0530

    Add files via upload

--- layouts/offers/Offers.js
+++ layouts/offers/Offers.js
@@ -5,8 +5,8 @@ define([
     'dojo/_base/declare',
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
-    'dojo/text!./template/offers.html'
-],function(declare,_WidgetBase,_TemplatedMixin,template){
+    'dojo/text!./template/offers.html',
+    ],function(declare,_WidgetBase,_TemplatedMixin,template){
     return declare('layouts.offers.Offers',[_WidgetBase,_TemplatedMixin],{
         templateString:template,
         postCreate:function(){
